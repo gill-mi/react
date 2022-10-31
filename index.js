bda0a7eb075c659ba1f1d069f1aca6bc79d2180e
gill-mi <d6618b@naver.com>
2022-10-31 18:16:04 +0900

    message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import PassingCount from "./chapter_10/PassingCount";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PageColor from "./wk10/PageColor";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<PassingCount />);
+root.render(<PageColor />);
 
